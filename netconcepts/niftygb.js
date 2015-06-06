beede7504d3e7e0d58edbf021167a4005f502334
mambax7 <mambax7@gmail.com>
2015-06-06 05:33:09 -0400

    Original files

---
+++ netconcepts/niftygb.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+if(!NiftyCheck())
+    return;
+
+Rounded("div#col1","tr tl br bl","#fff","#eeebdc");
+Rounded("div#col2","tr tl br bl","#fff","#eeebdc");
+Rounded("div#col3","tr tl br bl","#fff","#eeebdc");
+}
